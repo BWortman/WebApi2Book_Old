2554ce381f22d7f3608c4df6d89ebd8716884362
Brian Wortman <brian.wortman@outlook.com>
2014-06-13 17:44:08 -0400

    end of ch7

--- src/WebApi2Book.Web.Api/App_Start/NinjectConfigurator.cs
+++ src/WebApi2Book.Web.Api/App_Start/NinjectConfigurator.cs
@@ -19,6 +19,7 @@ using WebApi2Book.Web.Api.AutoMappingConfiguration;
 using WebApi2Book.Web.Api.MaintenanceProcessing;
 using WebApi2Book.Web.Api.Security;
 using WebApi2Book.Web.Api.InquiryProcessing;
+using WebApi2Book.Web.Api.LinkServices;
 
 namespace WebApi2Book.Web.Api
 {
@@ -54,6 +55,9 @@ namespace WebApi2Book.Web.Api
             container.Bind<IPagedDataRequestFactory>().To<PagedDataRequestFactory>().InSingletonScope();
             container.Bind<IAllTasksQueryProcessor>().To<AllTasksQueryProcessor>().InRequestScope();
             container.Bind<IAllTasksInquiryProcessor>().To<AllTasksInquiryProcessor>().InRequestScope();
+            container.Bind<ICommonLinkService>().To<CommonLinkService>().InRequestScope();
+            container.Bind<IUserLinkService>().To<UserLinkService>().InRequestScope();
+            container.Bind<ITaskLinkService>().To<TaskLinkService>().InRequestScope();
         }
 
         private void ConfigureLog4net(IKernel container)
