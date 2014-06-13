afa1a60bf66f390ad775a52f03f738d0f5a7476d
Brian Wortman <brian.wortman@outlook.com>
2014-06-13 17:09:48 -0400

    ch7, about to begin the Validation Using an Action Filter section

--- src/WebApi2Book.Web.Api.Models/Task.cs
+++ src/WebApi2Book.Web.Api.Models/Task.cs
@@ -1,20 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApi2Book.Web.Api.Models
 {
     public class Task : ILinkContaining
     {
         private List<Link> _links;
+        private bool _shouldSerializeAssignees;
 
+        [Key]
         public long? TaskId { get; set; }
+
+        [Editable(true)]
         public string Subject { get; set; }
+
+        [Editable(true)]
         public DateTime? StartDate { get; set; }
+
+        [Editable(true)]
         public DateTime? DueDate { get; set; }
+
+        [Editable(false)]
         public DateTime? CreatedDate { get; set; }
+
+        [Editable(false)]
         public DateTime? CompletedDate { get; set; }
+
+        [Editable(false)]
         public Status Status { get; set; }
+
+        [Editable(false)]
         public List<User> Assignees { get; set; }
+
+        [Editable(false)]
         public List<Link> Links
         {
             get { return _links ?? (_links = new List<Link>()); }
@@ -26,8 +45,6 @@ namespace WebApi2Book.Web.Api.Models
             Links.Add(link);
         }
 
-        private bool _shouldSerializeAssignees;
-
         public void SetShouldSerializeAssignees(bool shouldSerialize)
         {
             _shouldSerializeAssignees = shouldSerialize;
@@ -37,6 +54,5 @@ namespace WebApi2Book.Web.Api.Models
         {
             return _shouldSerializeAssignees;
         }
-
     }
 }
